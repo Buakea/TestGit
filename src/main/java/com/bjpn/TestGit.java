862842a56750ae80313d0e934ed5e3870dbdc755
Buakea <729161295@qq.com>
2023-04-03 18:50:32 +0800

    test push commit

--- src/main/java/com/bjpn/TestGit.java
+++ src/main/java/com/bjpn/TestGit.java
@@ -2,7 +2,6 @@ package com.bjpn;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("master");
-        System.out.println("我是hot-fix");
+        System.out.println("push");
     }
 }
