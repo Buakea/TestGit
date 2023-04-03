d436409588ea2e60285b5f4ed8ce7e9dfca78833
Buakea <729161295@qq.com>
2023-04-03 17:04:06 +0800

    hot-fix commit

--- src/main/java/com/bjpn/TestGit.java
+++ src/main/java/com/bjpn/TestGit.java
@@ -2,6 +2,6 @@ package com.bjpn;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("工号9527为您服务");
+        System.out.println("使用hotfix修改");
     }
 }
